19c33c5acb7e596cdfcce7161a23b5eb2127d925
leyuri <leyuri97@gmail.com>
2020-08-24 10:38:10 +0900

    environment setting 개발환경셋팅

---
+++ src/main/java/com/yuri/blog/test/BlogControllerTest.java
@@ -0,0 +1,22 @@
+package com.yuri.blog.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// 스프링이 com.yuri.blog 패키지 이하를 스캔해서 모든 파일을 메모리에 new 하는 것은 이나디
+// 특정 어노테이션이 붙어있는 클래스 파일들을 new 해서 (loC) 스프링 컨테이너에 관리해준다. 
+
+
+@RestController
+public class BlogControllerTest {
+	
+	//http://localhost:8080/test/hello
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>helloooooooo</h1>";
+	}
+	
+
+}
