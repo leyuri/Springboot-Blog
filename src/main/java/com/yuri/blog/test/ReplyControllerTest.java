ff93e0ec65a9de932927398f75c14ba136da0183
leyuri <leyuri97@gmail.com>
2020-09-07 13:40:32 +0900

    Add blocking of board infinite reference through JsonIgnoreProperties 무한참조 방지

---
+++ src/main/java/com/yuri/blog/test/ReplyControllerTest.java
@@ -0,0 +1,32 @@
+package com.yuri.blog.test;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yuri.blog.model.Board;
+import com.yuri.blog.model.Reply;
+import com.yuri.blog.repository.BoardRepository;
+import com.yuri.blog.repository.ReplyRepository;
+
+@RestController
+public class ReplyControllerTest {
+	
+	@Autowired
+	private BoardRepository boardRepository;
+	
+	@Autowired
+	private ReplyRepository replyRepository;
+	
+	@GetMapping("/test/board/{id}")
+	public Board getBoard(@PathVariable int id) {
+		return boardRepository.findById(id).get(); //jackson 라이브러리가 발동함(오브젝트를 json으로 리턴) => 모델이 getter를 호출 
+	}
+	@GetMapping("/test/reply")
+	public List<Reply> getReply() {
+		return replyRepository.findAll(); //jackson 라이브러리가 발동함(오브젝트를 json으로 리턴) => 모델이 getter를 호출 
+	}
+}
